cf1ec143cbb9abf0fc117b1cadd9260a1889fbfb
UraBoginskii <12345@gmail.com>
2022-12-17 21:25:33 +0300

    Задания по лекции 2

---
+++ Lection_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    
+    return result;
+    
+} 
+
+int a1 = 15;
+int b1 = 214;
+int c1 = 39;
+
+int max1 = max(a1, b1, c1);
+
+Console.WriteLine(max1);
---
+++ Lection_2_1/Program.cs
@@ -0,0 +1,24 @@
+﻿int max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    
+    return result;
+    
+} 
+
+int [] array = {11,21,3125,41,15,16,17,18,19};
+
+for (int i=1; i < 9; i++)
+
+Console.Write($"  {array[i]}");
+
+int max1 = max(
+    max(array[0], array[1], array[2]),
+    max(array[3], array[4], array[5]),
+    max(array[6], array[7], array[8]));
+
+Console.WriteLine();
+Console.WriteLine(max1);
